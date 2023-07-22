b83db0d1f209df2b228bfce01656d012cc03e5bd
Udit Jain <uditjain504@gmail.com>
2023-07-22 19:04:27 +0530

    Add promise handling

--- app.js
+++ app.js
@@ -20,68 +20,53 @@ const uri = "mongodb+srv://uj:wWbGuJwMi9y2cEn7@ujlist.w3ae9tm.mongodb.net/todoli
 mongoose.connect(uri)
 
 app.get("/", function (req, res) {
-    try {
-        res.redirect("/general");
-    } catch (err) {
-        console.log(err);
-    }
+    res.redirect("/general");
 });
 
 app.post("/category", function (req, res) {
-    try {
-        res.redirect("/" + req.body.categoryName.toLowerCase());
-    } catch (err) {
-        console.log(err);
-    }
+    res.redirect("/" + req.body.categoryName.toLowerCase());
 });
 
 app.post("/delete", async function (req, res) {
-    try {
-        const id = req.body.checkbox;
-        if (id) {
-            await Item.deleteOne({ _id: id });
-            res.redirect('back');
-        }
-    } catch (err) {
-        console.log(err);
+    const id = req.body.checkbox;
+    if (id) {
+        await Item.deleteOne({ _id: id }).then(function () {
+            console.log("Item deleted");
+        }).catch(function () {
+            console.log("Error while deleting item");
+        });
+        res.redirect('back');
     }
 });
 
 app.get("/:category", async function (req, res) {
-    try{
-        const category = req.params.category;
-        const items = await Item.find({category: category});
+    const category = req.params.category;
+    await Item.find({ category: category }).then(function (items) {
+        console.log("Items found: " + items);
         res.render("list", { items: items, category: _.capitalize(category) });
-    } catch (err) {
-        console.log(err);
-    }
+    }).catch(function () {
+        console.log("Error trying to find items for category: " + category);
+        res.redirect("/");
+    });
 });
 
 app.post("/:category", async function (req, res) {
-    try {
-        let category = req.body.category.toLowerCase();
-        if (req.body.newTodo != "") {
-            await Item.create({
-                name: req.body.newTodo,
-                category: category,
-                completed: false
-            });
-            res.redirect("/" + category);
-        }
-    } catch (err) {
-        console.log(err);
-    } 
+    let category = req.body.category.toLowerCase();
+    if (req.body.newTodo != "") {
+        await Item.create({ name: req.body.newTodo, category: category, completed: false }).then(function () {
+            console.log("Item created for category: " + category);
+        }).catch(function () {
+            console.log("Unable to create item for category: " + category);
+        })
+        res.redirect("/" + category);
+    }
 });
 
 app.get("/categories/list", async function (req, res) {
-    try{
-        Item.collection.distinct("category", function (error, results) {
-            console.log(results);
-        });
-        res.send("done");
-    } catch (err) {
-        console.log(err);
-    }
+    await Item.collection.distinct("category", function (error, results) {
+        console.log(results);
+    });
+    res.send("done");
 });
 
 app.listen(port, function () {
